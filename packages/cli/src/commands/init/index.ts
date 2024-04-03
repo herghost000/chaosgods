72bfa7d13c7073a9f1382ee568ab46c4b43ae958
小猪乱跑 <xiaozhulianpao@gmail.com>
2024-04-03 15:03:44 +0800

    fix(cli): 兼容tpls中新增的字段

--- packages/cli/src/commands/init/index.ts
+++ packages/cli/src/commands/init/index.ts
@@ -13,8 +13,9 @@ import { getProjectTpls } from '@/utils/github'
 import Package from '@/models/package'
 import { spawnAsync } from '@/utils/process'
 
-const TYPE_PROJECT = 1
-const TYPE_COMPONENT = 2
+const TAG_PROJECT = 'project'
+const TAG_COMPONENT = 'component'
+const TAG_TEMPLATE = 'template'
 
 export class InitCommand extends Command {
   public force: boolean = false
@@ -93,7 +94,7 @@ export class InitCommand extends Command {
     const files = await glob('**', {
       cwd: dir,
       nodir: true,
-      ignore: ['**/node_modules/**'],
+      ignore: ['**/node_modules/**', ...(this.tpl.ignores || [])],
     })
     return Promise.all(files.map(async (file) => {
       const filePath = path.resolve(dir, file)
@@ -133,71 +134,85 @@ export class InitCommand extends Command {
     }
     const info = await this.getProjectInfo()
     this.projectInfo = info
-    this.tpl = this.tpls[info.tpl]
+    this.tpl = this.tpls.filter(tpl => tpl.tags.includes(info.tag))[info.tpl]
     return info
   }
 
   public async getProjectInfo() {
     const ret = {
-      type: TYPE_PROJECT,
+      tag: TAG_PROJECT as keyof typeof names,
       name: '',
       version: '',
       tpl: 0,
     }
-    ret.type = await select({
+    const names = {
+      [TAG_PROJECT]: '项目',
+      [TAG_COMPONENT]: '组件',
+      [TAG_TEMPLATE]: '模版',
+    }
+    const descriptions = {
+      [TAG_PROJECT]: '查找快速创建项目相关模版仓库',
+      [TAG_COMPONENT]: '查找快速创建组件相关模版仓库',
+      [TAG_TEMPLATE]: '查找快速创建自定义模版相关模版仓库',
+    }
+    ret.tag = await select({
       message: '请选择创建类型',
-      default: TYPE_PROJECT,
+      default: TAG_PROJECT,
       choices: [
         {
-          name: '项目',
-          value: TYPE_PROJECT,
-          description: 'npm is the most popular package manager',
+          name: names[TAG_PROJECT],
+          value: TAG_PROJECT,
+          description: descriptions[TAG_PROJECT],
         },
         {
-          name: '组件',
-          value: TYPE_COMPONENT,
-          description: 'yarn is an awesome package manager',
+          name: names[TAG_COMPONENT],
+          value: TAG_COMPONENT,
+          description: descriptions[TAG_COMPONENT],
         },
+        {
+          name: names[TAG_TEMPLATE],
+          value: TAG_TEMPLATE,
+          description: descriptions[TAG_TEMPLATE],
+        },
+
       ],
     })
-    if (ret.type === TYPE_PROJECT) {
-      ret.name = await input({
-        message: '请输入项目名称',
-        default: this.projectName || 'project',
-        validate(value) {
-          return /^[a-zA-Z]+([_-]?[a-zA-Z0-9])*$/.test(value)
-        },
-      })
-      ret.version = await input({
-        message: '请输入项目版本号',
-        default: '1.0.0',
-        validate(value) {
-          return !!valid(value)
-        },
-        transformer(value, { isFinal }) {
-          if (isFinal)
-            return valid(value) || value
+    ret.name = await input({
+      message: '请输入项目名称',
+      default: this.projectName || 'project',
+      validate(value) {
+        return /^[a-zA-Z]+([_-]?[a-zA-Z0-9])*$/.test(value)
+      },
+    })
+    ret.version = await input({
+      message: '请输入项目版本号',
+      default: '1.0.0',
+      validate(value) {
+        return !!valid(value)
+      },
+      transformer(value, { isFinal }) {
+        if (isFinal)
+          return valid(value) || value
 
-          else
-            return value
-        },
-      })
+        else
+          return value
+      },
+    })
 
-      ret.tpl = await select({
-        message: '请选择项目模版',
-        default: 0,
-        choices: this.tpls.map((tpl, index) => {
-          return {
-            name: tpl.name,
-            value: index,
-            description: tpl.description,
-          }
-        }),
-      })
-    }
-    else {
-      log.info('a', this.tpls)
-    }
+    ret.tpl = await select({
+      message: `请选择相关${names[ret.tag]}`,
+      default: 0,
+      choices: this.tpls.filter((tpl) => {
+        return tpl.tags.includes(ret.tag)
+      }).map((tpl, index) => {
+        return {
+          name: tpl.name,
+          value: index,
+          description: tpl.description,
+        }
+      }),
+    })
+
     return ret
   }
 
