8822edd6e08a5f6a19d55f84dd466e58f530465a
小猪乱跑 <xiaozhulianpao@gmail.com>
2024-04-13 16:16:25 +0800

    ci: add autofix、automerge、cr

--- packages/cli/src/utils/__tests__/fs.spec.ts
+++ packages/cli/src/utils/__tests__/fs.spec.ts
@@ -2,12 +2,12 @@ import path from 'node:path'
 import { readFile } from '../fs'
 
 describe('cli/utils/fs.ts', () => {
-  it('通过readFile读取package.json文件', async () => {
+  it('应该通过readFile读取package.json文件', async () => {
     const pkg = readFile(path.resolve('package.json'))
     expect(pkg).toBeDefined()
     expect(pkg).not.toBeNull()
     expect(pkg).toBeTypeOf('string')
   })
 
-  it('通过writeFile写入文本到文件')
+  it('应该通过writeFile写入文本到文件')
 })
