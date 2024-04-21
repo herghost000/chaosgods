1720cc700fa391f3f3e66724154a34452909f4d4
小猪乱跑 <xiaozhulianpao@gmail.com>
2024-04-21 15:32:03 +0800

    build(vitest): 移除root下的vitest配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="vitest" />
+import { fileURLToPath } from 'node:url'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import { vitepressDemo } from 'vite-plugin-vitepress-demo'
+import vue from '@vitejs/plugin-vue'
 import { uiResolver } from './scripts/ui-resolver'
 import viteAlias from './scripts/vite-alias'
 
@@ -18,8 +22,18 @@ export default defineConfig({
       glob: ['**/demos/*.vue'],
     }),
     vueJsx(),
+    vue(),
   ],
   resolve: {
     alias: viteAlias,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    coverage: {
+      include: ['packages/**', '!packages/**/docs', '!packages/**/demos'],
+    },
+    root: fileURLToPath(new URL('./', import.meta.url)),
   },
 })
